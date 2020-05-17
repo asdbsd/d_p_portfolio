cb836c408461e40497c0bcab148898f19b4f0c6b
Dobriyan Petkov <dobriyanp@yahoo.com>
2020-05-17 11:39:40 +0700

    rendered tweets on tech-news page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
 
-  root to: 'pages#home'
-
-  resources :blogs do
-    member do
-      get :change_status
-    end
-  end
-
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_up: 'register', sign_out: 'logout' }
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
@@ -18,6 +10,15 @@ Rails.application.routes.draw do
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'tech-news', to: 'pages#tech_news'
+
+  resources :blogs do
+    member do
+      get :change_status
+    end
+  end
+
+  root to: 'pages#home'
 
 
 end
