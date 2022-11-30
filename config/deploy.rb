cde24ad36b694bb16eb853aa95f6f7094c47ab98
Dobriyan Petkov <dobriyanp@yahoo.com>
2022-11-30 21:38:43 +0000

    updated capistrano deployment data

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,10 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.1"
 
 set :application, "d_p_portfolio"
 set :repo_url, "git@github.com:asdbsd/d_p_portfolio.git"
-
+set :branch, "capistrano-deploy"
 set :deploy_to, "/home/deploy/#{fetch :application}"
-
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
-
-set :keep_releases, 5
+append :linked_files, "config/master.key"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
